9f5563e1bbe8502dec8078686cf14b70326039bd
Abdellah Fanidi <afanidi@e1r2p12.1337.ma>
2024-10-30 15:45:03 +0100

    add buildRes and sendData function

--- HttpResponse/HttpResponse.hpp
+++ HttpResponse/HttpResponse.hpp
@@ -8,8 +8,25 @@ private:
     std::map<int, std::string> Pages;
     
 public:
-    HttpResponse();
-    void GenerateResponse(const Request& request);
-    void setReasonPhrase(int statusCode);
-    void LoadErrorPage();
+    HttpResponse(int code, std::string url);
+    void LoadPage();
+
+    std::vector<uint8_t> buildResponseBuffer(); // this for building and set it in send syscall
 };
+
+// send data !!
+bool sendData(int fd, const std::vector<uint8_t>& data)
+{
+    size_t totalBytesSent = 0;
+    size_t dataSize = data.size();
+
+    while (totalBytesSent < dataSize)
+    {
+        ssize_t bytesSent = send(fd, &data[totalBytesSent], dataSize - totalBytesSent, 0);
+        if (bytesSent <= 0) {
+            return false; // Error occurred, stop sending and return false
+        }
+        totalBytesSent += bytesSent;
+    }
+    return true;
+}
