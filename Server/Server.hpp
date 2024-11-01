353c347566a413528f85cbcb6b71019c797605c2
Oussama Sabbar <ousabbar@e1r5p11.1337.ma>
2024-11-01 22:55:52 +0100

    add new getters and setters and add new redirect data

--- Server/Server.hpp
+++ Server/Server.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include "Route.hpp"
-#include "Connection.hpp"
+#include "../Route/Route.hpp"
+// #include "Connection.hpp"
 
 class Server {
 private:
@@ -8,17 +8,42 @@ private:
     std::vector<int> ports;
     std::vector<std::string> serverNames; 
     std::string serverRoot;
-    std::map<int, Connection*> connections;
+    // std::map<int, Connection*> connections;
     std::map<std::string, Route> routes;
     std::map<int, std::string> errorPages;
     size_t clientMaxBodySize;
-    bool isDefault;
+    // bool isDefault;
+    std::string redirectnewPath;
+    std::string redirectPathOldPath;
+    int redirectCode;
 
 public:
-    Server(const std::string& host, const std::vector<int>& ports) 
-        : host(host), ports(ports) {}
+    void hostSetter(std::string host);
+    std::string hostGetter();
+    void portSetter(int port);
+    std::vector<int> &portGetter();
+    void serverNamesSetter(std::string serverName);
+    std::vector<std::string> serverNamesGetter();
+    void serverRootSetter(std::string serverRoot);
+    std::string serverRootGetter();
+    void errorPagesSetter(int errorCode, std::string errorPage);
+    std::map<int, std::string> errorPagesGetter();
+    void clientMaxBodySizeSetter(size_t clientMaxBodySize);
+    size_t clientMaxBodySizeGetter();
+    void addRoute(Route newRoute);
+    Route& getRoute(const std::string& path);
+    void setRedirectnewPath(std::string redirectnewPath);
+    std::string getRedirectnewPath();
+    void setRedirectPathOldPath(std::string redirectPathOldPath);
+    std::string getRedirectPathOldPath();
+    void setRedirectCode(int redirectCode);
+    int getRedirectCode();
+    std::map<std::string, Route>& getRoutes();
+    // Server();
+    // Server(const std::string& host, const std::vector<int>& ports) 
+    //     : host(host), ports(ports) {}
 
-    bool setup(); // Sets up the server
-    bool listen(); // Starts listening for connections
-    void addNewConnection(Connection newConnection);
+    // bool setup(); // Sets up the server
+    // bool listen(); // Starts listening for connections
+    // void addNewConnection(Connection newConnection);
 };
