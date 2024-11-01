3fa7e668e88605ee39532d0a1235caddc8451b3d
Oussama Sabbar <ousabbar@e1r5p11.1337.ma>
2024-11-01 22:54:46 +0100

    fixing some errors in the Webserver.hpp and adding new methods

--- WebServers/Webservers.hpp
+++ WebServers/Webservers.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "Server.hpp"
+#include "../Server/Server.hpp"
 
 
 class WebServer {
@@ -7,9 +7,12 @@ private:
     std::map<int, Server> servers; // List of servers parsed from configuration int is like an ID or somthing ti identify the Server
 
 public:
-    WebServer(const std::string& configFile);
-    bool initialize(); // Initializes the servers from config file
-    void run(); // run the Main servers loop
-    void addNewServer(Server& newServer);
+    void addServer(Server& newServer, int identifier);
     Server& getServer(int identifier);
+    std::map<int, Server>& getServers();
+    // WebServer(const std::string& configFile);
+    // bool initialize(); // Initializes the servers from config file
+    // void run(); // run the Main servers loop
+    // void addNewServer(Server& newServer);
+    // Server& getServer(int identifier);
 };
