d70c9b1fef0bc10405678a7f576bb47809e90e6e
Oussama Sabbar <ousabbar@e1r5p11.1337.ma>
2024-11-01 22:58:35 +0100

    commonts

--- HttpMessage/HttpMessage.hpp
+++ HttpMessage/HttpMessage.hpp
@@ -13,26 +13,26 @@
 #include <cstring>
 
 
-class HttpMessage {
-protected:
-    std::map<std::string, std::string> headers;
-    std::vector<uint8_t> body; // Raw body data
-    std::string version;
-    // we can just version "HTTP/1.1" , sinmon  :  400 Bad Request ...
+// class HttpMessage {
+// protected:
+//     std::map<std::string, std::string> headers;
+//     std::vector<uint8_t> body; // Raw body data
+//     std::string version;
+//     // we can just version "HTTP/1.1" , sinmon  :  400 Bad Request ...
 
-public :
-    HttpMessage();
-// this after validation of header , can store it in map container
-    void addHeader(const std::string& key, const std::string& value);
+// public :
+//     HttpMessage();
+// // this after validation of header , can store it in map container
+//     void addHeader(const std::string& key, const std::string& value);
 
-    std::string getHeader(const std::string& key) const;
+//     std::string getHeader(const std::string& key) const;
 
-    std::string getVersion() const;
+//     std::string getVersion() const;
  
-    virtual std::string toString() const;
-    void resetMessage(){
-        version.clear();
-        body.clear();
-        headers.clear();
-    }
-};
+//     virtual std::string toString() const;
+//     void resetMessage(){
+//         version.clear();
+//         body.clear();
+//         headers.clear();
+//     }
+// };
