add7d576fd630fbec552f0ff27f537736223160a
Abdellah Fanidi <afanidi@c4r9p10.1337.ma>
2024-11-02 19:37:05 +0100

    add cgi

--- HttpResponse/HttpResponse.cpp
+++ HttpResponse/HttpResponse.cpp
@@ -15,6 +15,74 @@
 //5xx (Server Error): The server failed to fulfill an apparently
 //valid request
 
+bool checkIfCGI(const std::string& url)
+{
+    size_t extPos = url.rfind('.');
+    if (extPos != std::string::npos)
+    {
+        std::string extension = url.substr(extPos);
+        return cgiExtensions.find(extension) != cgiExtensions.end();
+    }
+    return false;
+}
+
+std::vector<char*> HttpResponse::createEnvChar(const std::string& Url, ...) const 
+{
+    std::vector<char*> envVars;
+    bool cookies = false; // TODO detect if have cookies session manangement
+    
+    std::string authType = cookies ? "SessionCookie" : "";
+    envVars.push_back(strdup(("AUTH_TYPE=" + authType).c_str()));
+
+    std::string method = "GET"; // FROM PARAM FUNCTION
+    envVars.push_back(strdup(("CONTENT_LENGTH=" + intToString(request.GetBody.size())).c_str())); // Length of POST data
+    
+    
+
+    return envVars;
+}
+void HttpResponse::executeCGI(const std::string& scriptPath,std::vector<char*> &envp)
+{
+    
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return;
+    }
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return;
+    }
+    if (pid == 0)
+    {
+        // In child process
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]); 
+        char* argv[] = {const_cast<char*>(scriptPath.c_str()), NULL};
+        execve(scriptPath.c_str(), argv, &envp[0]); // envp.data() but is in c++11
+    }
+    else
+    {
+        // In parent process
+        close(pipefd[1]);
+        std::string cgiOutput;
+        char buffer[1024];
+        ssize_t bytesRead;
+         while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0)
+            cgiOutput.append(buffer, buffer + bytesRead);
+        close(pipefd[0]);
+        waitpid(pid, NULL, 0);
+        body.assign(cgiOutput.begin(), cgiOutput.end());
+    }
+}
+
+std::string createSetCookieHeader(const std::string& sessionId)
+{
+        return "session_id=" + sessionId + "; Path=/; HttpOnly";
+}
+
 std::string intToString(size_t number)
 {
     std::stringstream ss;
@@ -39,9 +107,9 @@ std::string getCurrentTimeFormatted()
 
 
 HttpResponse::HttpResponse(int index_connection, const Server &server):Pages(server.getErrorPages())
-{
-    // i
-    statusCode = server.request.GetCode();
+{ 
+    // id of connection to get request also get same data  from config file 
+    statusCode = server.connections[index_connection].request.GetStatusCode();
     switch (statusCode)
     {  
         case 400: reasonPhrase = "Bad Request"; break;
@@ -50,46 +118,27 @@ HttpResponse::HttpResponse(int index_connection, const Server &server):Pages(ser
         case 405: reasonPhrase = "Method Not Allowed"; break;
         case 500: reasonPhrase = "Internal Server Error"; break;
         case 505: reasonPhrase = "HTTP Version Not Supported"; break;
-        default: Pages[statusCode] = request.GetUrl(); break;
+        default: Pages[statusCode] = server.connections[index_connection].request.GetUrl(); break;
     }
+
+    // store mime types 
+    mimeTypes["html"] = "text/html";
+    mimeTypes["css"] = "text/css";
+    mimeTypes["js"] = "application/javascript";
+    mimeTypes["json"] = "application/json";
+    mimeTypes["xml"] = "application/xml";
+    mimeTypes["jpg"] = "image/jpeg";
+    mimeTypes["jpeg"] = "image/jpeg";
+    mimeTypes["png"] = "image/png";
+    mimeTypes["gif"] = "image/gif";
+    mimeTypes["svg"] = "image/svg+xml";
+    mimeTypes["txt"] = "text/plain";
+    mimeTypes["pdf"] = "application/pdf";
+    mimeTypes["zip"] = "application/zip";
+    mimeTypes["mp3"] = "audio/mpeg";
+    mimeTypes["mp4"] = "video/mp4";
+
 }
-
-void HttpResponse::buildingHeaders()
-{
-
-}
-// HttpResponse::HttpResponse(const HttpRequest &request, const Server &server)
-// {
-
-//     contractor just for preparation all data to need
-//     add also pages default 200 with url (it will be updated every request possible!!)
-//     dynamic error from config file else we be default pages
-//     Pages[400] = "docs/errorPages/400.html";
-//     Pages[403] = "docs/errorPages/403.html";
-//     Pages[404] = "docs/errorPages/404.html";
-//     Pages[405] = "docs/errorPages/405.html";
-//     Pages[500] = "docs/errorPages/500.html";
-//     Pages[505] = "docs/errorPages/505.html";
-//     statusCode = code;
-//     switch (statusCode)
-//     {  
-//         case 200: reasonPhrase = "OK"; Pages[code] = url; break;
-//         case 400: reasonPhrase = "Bad Request"; break;
-//         case 403: reasonPhrase = "Forbidden"; break;
-//         case 404: reasonPhrase = "Not Found"; break;
-//         case 405: reasonPhrase = "Method Not Allowed"; break;
-//         case 500: reasonPhrase = "Internal Server Error"; break;
-//         case 505: reasonPhrase = "HTTP Version Not Supported"; break;
-//         default: reasonPhrase = "Unknown Status"; break;
-//     }
-//     addHeader("Content-Type", "text/html"); 
-//         LoadPage();
-//     //addHeader("Connection", "close"); // check just from request header
-//     // after we have body now we need set content-length
-//     addHeader("Content-Length", intToString(body.size()));
-//     addHeader("Date", getCurrentTimeFormatted());
-//     addHeader("Server", "WebServ 1337");
-// }
 
 void HttpResponse::LoadPage()
 {
@@ -101,6 +150,46 @@ void HttpResponse::LoadPage()
         if (file.is_open())
             body.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
     }
+}
+
+// set best type of mime for content-type !!
+std::string HttpResponse::getMimeType(const std::string& filePath) const
+{
+    size_t pos = filePath.find_last_of('.');
+    if (pos == std::string::npos)
+        return "application/octet-stream";
+    std::string extension = filePath.substr(pos + 1);
+    std::map<std::string, std::string>::const_iterator it = mimeTypes.find(extension);
+    if (it != mimeTypes.end())
+        return it->second;
+    return "application/octet-stream";  // Default type if extension not found
+}
+
+void HttpResponse::generateResponse(std::string Url, bool flagConnection,... )
+{
+    // detect CGI    
+    if (checkIfCGI(Url))
+        executeCGI(Url, createEnvMap(Url));  // createEnvMap builds necessary env variables
+    else
+        LoadPage();
+    addHeader("Content-Type", getMimeType(Url));
+    
+    //Transfer-Encoding: chunked or content-length ?
+    addHeader("Content-Length", intToString(body.size()));
+    
+    
+    addHeader("Date", getCurrentTimeFormatted());
+    addHeader("Server", "WebServ 1337");  
+    addHeader("Connection",flagConnection ? "keep-alive" : "close");
+
+    //Location: https://example.com/new-path
+    // from config file  : redirect: old-path 3xx new-path
+    //The browser automatically makes a new HTTP request to the URL specified in the Location header
+    if (statusCode > 299 && statusCode < 400)
+        addHeader("Location", "localhost?" + "new-path");
+
+    // cookies header ?
+    // addHeader("Set-Cookie",createSetCookieHeader());
 }
 
 
--- HttpResponse/HttpResponse.hpp
+++ HttpResponse/HttpResponse.hpp
@@ -6,6 +6,11 @@ private:
     int statusCode;
     std::string reasonPhrase;
     std::map<int, std::string> Pages;
+    //std::map<std::string, std::map<std::string, std::string>> sessions;
+    std::map<std::string, std::string> mimeTypes;
+    std::set<std::string> cgiExtensions = { ".php", ".py"};
+
+
     
 public:
    HttpResponse::HttpResponse(const HttpRequest &request, const Server &server);
