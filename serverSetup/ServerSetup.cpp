43cfae5135bf14630f1efab7776bb138518d9dd6
oussamaGUN <sebbaroussama3@gmail.com>
2024-11-06 19:22:27 +0100

    updates

--- serverSetup/ServerSetup.cpp
+++ serverSetup/ServerSetup.cpp
@@ -1,8 +1,10 @@
 #include "ServerSetup.hpp"
 #include <signal.h>
-// #include <csignal> 
+#include <ctime>
+#include <errno.h>
 
 #define MAX_CLIENTS 10
+
 void ServerSetup(ParsingConfig &Config)
 {
 	std::map<int, Server> Servers = Config.webServer.getServers();
@@ -43,23 +45,12 @@ void ServerSetup(ParsingConfig &Config)
 			pfd.events = POLLIN;           
 			pollDescriptorsByServer.push_back(pfd);
 		}
-		// fdsServer[it->second.hostGetter()] = pollDescriptorsByServer;
 	}
-	// for (size_t i = 0; i < pollDescriptorsByServer.size() && i < serverPorts.size(); i++)
-	// {
-	// 	std::vector<struct pollfd> itt = pollDescriptorsByServer[i];
-	// 	std::vector<int> ports = serverPorts[i];
-	// 	for (size_t j = 0; j < itt.size(); j++)
-	// 	{
-	// 		std::cout << ports[i] << ": " <<  itt[j].fd << " | ";
-	// 	}
-	// 	std::cout << std::endl;
-	// }
-	
+
 	size_t lastServerSocket = pollDescriptorsByServer.size();
+	// time_t time;
 	while (1)
 	{
-		signal(SIGPIPE, SIG_IGN);
 		int pollfds = poll(pollDescriptorsByServer.data(), pollDescriptorsByServer.size(), 1);
 		if (pollfds < 0)
 		{
@@ -72,42 +63,87 @@ void ServerSetup(ParsingConfig &Config)
 			{
 				// hna kantchekiw wach server tra fih chi event in case tra event khassna acceptiw new connection
 				// 
+				
 				if (pollDescriptorsByServer[index].revents & POLLIN)
 				{
 					int clientSocket = accept(pollDescriptorsByServer[index].fd, NULL , NULL);
 					if (clientSocket < 0)
 						break;
 					std::cout << "new conection\n";
+					
 					struct pollfd ClientPollFd;
+					// means that we are interested in reading from this client
 					ClientPollFd.events = POLLIN;
 					ClientPollFd.fd = clientSocket;
+
 					pollDescriptorsByServer.push_back(ClientPollFd);
+
+					char buffer[1024];
+					/*
+						flags:
+							MSG_OOB:For retrieving urgent data sent with send(MSG_OOB)
+							MSG_PEEK:For peeking at the incoming data without removing it from the input queue
+							MSG_WAITALL:For blocking until the full amount of data is received
+							0: regular behavior
+					*/
+					int bytesRead = recv(pollDescriptorsByServer.back().fd, buffer, sizeof(buffer), 0);
+					if (bytesRead > 0)
+					{
+						std::cout << "Request received from first connection: " << buffer << std::endl;
+					}
+					else if (bytesRead == 0)
+					{
+						close(pollDescriptorsByServer[index].fd);
+						pollDescriptorsByServer.erase(pollDescriptorsByServer.begin() + index);
+						std::cout << "Client disconnected\n";
+					}
+					memset(buffer, 0, sizeof(buffer));
+					// so that we can send a response to the client
+					pollDescriptorsByServer.back().revents = POLLOUT;
+				}
+				if (pollDescriptorsByServer.back().revents & POLLOUT)
+				{
 					const char* httpResponse = 
 					"HTTP/1.1 200 OK\r\n"
 					"Content-Length: 13\r\n"
 					"\r\n"
 					"Connection OK!";
-					send(pollDescriptorsByServer.back().fd, httpResponse, strlen(httpResponse), 0);
+					send(pollDescriptorsByServer.back().fd, httpResponse, strlen(httpResponse), MSG_NOSIGNAL);
+					// set revenets to POLLIN because i want to keep reading from the client
+					pollDescriptorsByServer.back().revents = POLLIN;
 				}
 			}
 			else
 			{
 				// had l condition katchki ala kola client wach fih chi 7aja mat reada in case makanch walo
 				// ghadi checkiha later 7itach socket non-blocking
-				char buffer[1024];
-				int bytesRead = recv(pollDescriptorsByServer[index].fd, buffer, sizeof(buffer), 0);
-				if (bytesRead > 0)
+				if (pollDescriptorsByServer[index].revents & POLLIN)
 				{
-					std::cout << "Request received: " << buffer << std::endl;
-					// hna fin ghadi tsendi response to the same client
+					char buffer[1024];
+					int bytesRead = recv(pollDescriptorsByServer[index].fd, buffer, sizeof(buffer), 0);
+					if (bytesRead > 0)
+					{
+						std::cout << "Request received from second connection: " << buffer << std::endl;
+					}
+					else if (bytesRead == 0)
+					{
+						close(pollDescriptorsByServer[index].fd);
+						pollDescriptorsByServer.erase(pollDescriptorsByServer.begin() + index);
+						std::cout << "Client disconnected\n";
+					}
+					memset(buffer, 0, sizeof(buffer)); // bach nfreei lbuffer
+					pollDescriptorsByServer[index].revents = POLLOUT;
 				}
-				else if (bytesRead == 0)
+				if (pollDescriptorsByServer[index].revents & POLLOUT)
 				{
-					close(pollDescriptorsByServer[index].fd);
-					pollDescriptorsByServer.erase(pollDescriptorsByServer.begin() + index);
-					std::cout << "Client disconnected\n";
+					const char* httpResponse = 
+					"HTTP/1.1 200 OK\r\n"
+					"Content-Length: 13\r\n"
+					"\r\n"
+					"Connection OK second!";
+					send(pollDescriptorsByServer[index].fd, httpResponse, strlen(httpResponse), MSG_NOSIGNAL);
+					pollDescriptorsByServer[index].revents = POLLIN;
 				}
-				memset(buffer, 0, sizeof(buffer)); // bach nfreei lbuffer
 			}
 		}
 	}
--- serverSetup/client.cpp
+++ serverSetup/client.cpp
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 int main() {
-    const char* serverIP = "127.0.0.3";  
+    const char* serverIP = "127.0.0.1";  
     const int serverPort = 8080;       
 
     
@@ -31,14 +31,24 @@ int main() {
     }
 
     const char* request =  "hello\r\n";
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 10; ++i) {
         if (send(sock, request, strlen(request), 0) < 0) {
             std::cerr << "Send failed\n";
             close(sock);
             return -1;
         }
+        char buffer[1024];
+		int bytesRead = recv(sock, buffer, sizeof(buffer), 0);
+		if (bytesRead > 0)
+		{
+			std::cout << "Request received: " << buffer << std::endl;
+			// hna fin ghadi tsendi response to the same client
+		}
         std::cout << "Request sent " << i + 1 << " times\n";
-        sleep(2);
+        std::cout << std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
+        sleep(1);
     }
     
     close(sock);
