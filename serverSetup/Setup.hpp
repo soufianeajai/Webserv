658ca35c6007647b7105d4200bc5e49cc6787c78
oussamaGUN <sebbaroussama3@gmail.com>
2024-11-06 10:59:58 +0100

    update

---
+++ serverSetup/Setup.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <poll.h>
+
+void ServerSetup();
+
