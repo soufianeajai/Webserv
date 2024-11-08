59192d9707b9023af5a42653d9906fc2e052af82
oussamaGUN <sebbaroussama3@gmail.com>
2024-11-08 19:53:08 +0100

    more parsing

--- ParsingConfig/ParsingConfig.cpp
+++ ParsingConfig/ParsingConfig.cpp
@@ -13,6 +13,12 @@ int numberConversion(std::string &string)
     } 
     return number;
 }
+void ft_error(std::string err, std::ifstream& fd)
+{
+    fd.close();
+    std::cerr << err << std::endl;
+    exit (EXIT_FAILURE);
+}
 bool locationBlock(Server &server, std::ifstream &FILE, std::vector<std::string> locationPath)
 {
     std::string str;
@@ -21,17 +27,23 @@ bool locationBlock(Server &server, std::ifstream &FILE, std::vector<std::string>
     if (locationPath.size() < 2)
         locationPath.push_back("/");
     else if (locationPath.size() > 2)
-    {
-        std::cout << "Error: Invalid location" << std::endl;
-        exit(1);
-    }
+        ft_error("Error: Invalid location", FILE);
     route.setPath(locationPath[1]);
-    // std::cout << route.getPath() << std::endl;
+
     while (getline(FILE, str))
     {
         if (!str.empty())
         {
-            if (str.find("location") != std::string::npos)
+            std::stringstream ss(str);
+            std::string to;
+            std::vector<std::string> arr;
+            while (getline(ss, to, ' '))
+            {
+                if (!to.empty())
+                    arr.push_back(to);
+            }
+
+            if (arr[0] == "location:")
             {
                 std::stringstream ss(str);
                 std::string to;
@@ -42,11 +54,10 @@ bool locationBlock(Server &server, std::ifstream &FILE, std::vector<std::string>
                         arr.push_back(to);
                 }
                 server.addRoute(route);
-                // std::cout << "end location------->" << std::endl;
-                if (locationBlock(server, FILE, arr))  // Check if "server" was found in the recursive call
-                    return true;  // Stop further execution if "server" was found
+                if (locationBlock(server, FILE, arr))
+                    return true;
             }
-            else if (str == "server")
+            else if (arr[0] == "server")
             {
 
                 FILE.seekg(-str.length() - 1, std::ios_base::cur); // Go back one line
@@ -57,11 +68,10 @@ bool locationBlock(Server &server, std::ifstream &FILE, std::vector<std::string>
                 server.addRoute(route);
                 return true;  // Indicate that "server" was found to all recursive calls
             }
-            else if (str.find("host") != std::string::npos || str.find("port") != std::string::npos 
-                || str.find("server_names") != std::string::npos || str.find("server_root") != std::string::npos 
-                || str.find("error_page") != std::string::npos || str.find("client_body_size") != std::string::npos)
+            else if (arr[0] == "host:" || arr[0] == "port:"
+                || arr[0] == "server_names:" || arr[0] == "server_root:"
+                || arr[0] == "error_page:" || arr[0] == "client_body_size:")
             {
-
                 FILE.seekg(-str.length() - 1, std::ios_base::cur); // Go back one line
                 if (FILE.fail()) {
                     FILE.clear();
@@ -72,22 +82,10 @@ bool locationBlock(Server &server, std::ifstream &FILE, std::vector<std::string>
             }
             else
             {
-                    std::stringstream ss(str);
-                    std::string to;
-                    std::vector<std::string> arr;
-                    while (getline(ss, to, ' '))
-                    {
-                        if (!to.empty())
-                            arr.push_back(to);
-                    }
-
-                    if (arr[0].find("methods") != std::string::npos)
+                    if (arr[0] == "methods:")
                     {
                         if (arr.size() < 2 || arr.size() > 4)
-                        {
-                            std::cout << "Error: Invalid methods" << std::endl;
-                            exit(1);
-                        }
+                            ft_error("Error: Invalid methods", FILE);
                         for (size_t i = 1; i < arr.size(); i++)
                         {
                             if (arr[i] != "GET" && arr[i] != "POST" && arr[i] != "DELETE")
@@ -97,117 +95,63 @@ bool locationBlock(Server &server, std::ifstream &FILE, std::vector<std::string>
                             }
                             route.addAllowedMethod(arr[i]);
                         }
-                        // std::set<std::string> methods = route.getAllowedMethods();
-                        // for (std::set<std::string>::iterator it = methods.begin(); it != methods.end(); it++)
-                        // {
-                        //     std::cout << "|" << *it << "|";
-                        // }
-                        // std::cout << std::endl;
                     }
-                    else if (arr[0].find("root") != std::string::npos)
+                    else if (arr[0] == "root:")
                     {
                         if (arr.size() != 2)
-                        {
-                            std::cout << "Error: Invalid root" << std::endl;
-                            exit(1);
-                        }
+                            ft_error("Error: invalid root", FILE);
                         route.setRoot(arr[1]);
-                        // std::cout << "|" << route.getRoot() << "|" << std::endl;
                     }
-                    else if (arr[0].find("default_file") != std::string::npos)
+                    else if (arr[0] == "default_file:")
                     {
                         if (arr.size() != 2)
-                        {
-                            std::cout << "Error: Invalid default_file" << std::endl;
-                            exit(1);
-                        }
+                            ft_error("Error: Invalid default_file", FILE);
                         route.setDefaultFile(arr[1]);
-                        // std::cout << "|" << route.getDefaultFile() << "|" << std::endl;
                     }
-                    else if (arr[0].find("autoindex") != std::string::npos)
+                    else if (arr[0] == "autoindex:")
                     {
-                        if (arr.size() != 2)
-                        {
-                            std::cout << "Error: Invalid autoindex" << std::endl;
-                            exit(1);
-                        }
-                        if (arr[1] != "on" && arr[1] != "off")
-                        {
-                            std::cout << "Error: Invalid autoindex" << std::endl;
-                            exit(1);
-                        }
+                        if (arr.size() != 2 || (arr[1] != "on" && arr[1] != "off"))
+                            ft_error("Error: invalid autoindex", FILE);
                         if (arr[1] == "on")
                             route.setAutoindex(true);
                         else if (arr[1] == "off")
                             route.setAutoindex(false);
                         else
-                        {
-                            std::cout << "Error: Invalid autoindex" << std::endl;
-                            exit(1);
-                        }
-                        // if (route.getAutoindex())
-                            // std::cout << "|" << "true" << "|" << std::endl;
-                        // else
-                            // std::cout << "|" << "false" << "|" << std::endl;
+                            ft_error("Error: invalid autoindex", FILE);
                     }
-                    else if (arr[0].find("cgi_extension") != std::string::npos)
+                    else if (arr[0] == "cgi_extension:")
                     {
                         if (arr.size() < 2)
-                        {
-                            std::cout << "Error: Invalid cgi_extension" << std::endl;
-                            exit(1);
-                        }
+                            ft_error("Error: invalid cgi extension", FILE);
                         for (size_t i = 1; i < arr.size(); i++)
                         {
                             route.addCgiExtension(arr[i]);
                         }
-                        std::set<std::string> cgiExtensions = route.getCgiExtensions();
-                        for (std::set<std::string>::iterator it = cgiExtensions.begin(); it != cgiExtensions.end(); it++)
-                        {
-                            // std::cout << "|" << *it << "|";
-                        }
-                        // std::cout << std::endl;
                     }
-                    else if (arr[0].find("redirect") != std::string::npos)
+                    else if (arr[0] == "redirect:")
                     {
                         if (arr.size() != 3)
-                        {
-                            std::cout << "Error: Invalid redirect" << std::endl;
-                            exit(1);
-                        }
+                            ft_error("Error: invalid redirect", FILE);
                         int i = 0;
                         while (arr[2][i])
                         {
                             if (!isdigit(arr[2][i]))
-                            {
-                                std::cout << "Error: Invalid redirect status code" << std::endl;
-                                exit(1);
-                            }
+                                ft_error("Error: invalid redirect status code", FILE);
                             i++;
                         }
                         route.setIsRedirection(true);
                         route.setRedirectnewPath(arr[1]);
                         route.setRedirectCode(numberConversion(arr[2]));
-                        // std::cout << "|" << server.getRedirectnewPath() << "|" << server.getRedirectPathOldPath() << "|" << server.getRedirectCode() << "|" << std::endl;
                     }
                     else
-                    {
-                        std::cout << "Error: Invalid config file" << std::endl;
-                        exit(1);
-                    }
-
+                        ft_error("Error: " + arr[0], FILE);
             }
         }
     }
     server.addRoute(route);
     return false;  // Continue if "server" was not found
 }
-void ft_error(std::string err, std::ifstream& fd)
-{
-    fd.close();
-    std::cerr << err << std::endl;
-    exit (EXIT_FAILURE);
-}
+
 bool ParsingConfig::hostCheck(std::string host)
 {
     std::stringstream ss(host);
@@ -255,7 +199,6 @@ ParsingConfig parsingConfig(const char *configFile)
         if (str == "server") // Check if "server" was found in the recursive call
         {
             Server server;
-            // std::cout << "server" << std::endl;
             while ((getline(FILE, str) && str != "server"))
             {
                 serverFlag = 0;
@@ -271,13 +214,13 @@ ParsingConfig parsingConfig(const char *configFile)
                     }
                     if (!arr.size())
                         break;
-                    if (arr[0].find("host") != std::string::npos)
+                    if (arr[0] == "host:")
                     {
                         if (arr.size() != 2 || !parsingConfig.hostCheck(arr[1]))
                             ft_error("invalid host", FILE);
                         server.hostSetter(arr[1]);
                     }
-                    else if (arr[0].find("port") != std::string::npos)
+                    else if (arr[0] == "port:")
                     {
                         if (arr.size() < 2 || arr[1] == " ")
                             ft_error("Error: invalid port", FILE);
@@ -290,7 +233,7 @@ ParsingConfig parsingConfig(const char *configFile)
                             server.portSetter(port);
                         }
                     }
-                    else if (arr[0].find("server_names") != std::string::npos)
+                    else if (arr[0] == "server_names:")
                     {
                         if (arr.size() < 2)
                             ft_error("Error: invalid server names", FILE);
@@ -300,13 +243,13 @@ ParsingConfig parsingConfig(const char *configFile)
                         }
                         std::vector<std::string> serverNames = server.serverNamesGetter();
                     }
-                    else if (arr[0].find("server_root") != std::string::npos)
+                    else if (arr[0] == "server_root:")
                     {
                         if (arr.size() != 2)
                             ft_error("Error: Invalid server_root", FILE);
                         server.serverRootSetter(arr[1]);
                     }
-                    else if (arr[0].find("error_page") != std::string::npos)
+                    else if (arr[0] == "error_page:")
                     {
                         if (arr.size() != 3)
                             ft_error("Error: Invalid error_page", FILE);
@@ -323,38 +266,21 @@ ParsingConfig parsingConfig(const char *configFile)
                             ft_error("Error: invalid error page code", FILE);
                         server.errorPagesSetter(numberConversion(arr[1]), arr[2]);
                     }
-                    else if (arr[0].find("client_body_size") != std::string::npos)
+                    else if (arr[0] == "client_body_size:")
                     {
-                        
                         if (arr.size() != 2 || !parsingConfig.checkClientBodySize(arr[1]))
-                        {
-                            std::cout << "Error: Invalid client_body_size" << std::endl;
-                            exit(1);
-                        }
-                        
+                            ft_error("Error: invalid client body size", FILE);
                         server.clientMaxBodySizeSetter(numberConversion(arr[1]));
-                        // std::cout << "|" << server.clientMaxBodySizeGetter() << "|" << std::endl;
                     }
-                    else if (arr[0].find("location") != std::string::npos)
+                    else if (arr[0] == "location:")
                     {
-
                         if (locationBlock(server, FILE, arr) && serverFlag == 1) // check dyal serverFlag ila kant chi location block flowel dyal server
                             break;
                     }
                     else
-                    {
-                        std::cout << "Error: Invalid config file" << std::endl;
-                        exit(1);
-                    }
+                        ft_error("Error: " + arr[0], FILE);
                 }
             }
-            // std::cout << "-----------" << std::endl;
-            // std::map<int, std::string> errorPages = server.errorPagesGetter();
-            // for (std::map<int, std::string>::iterator it = errorPages.begin(); it != errorPages.end(); it++)
-            // {
-                // std::cout << "|" << it->first << "|" << it->second << "|" << std::endl;
-            // }
-            // std::cout << "-----------" << std::endl;
             parsingConfig.webServer.addServer(server);
         }    
     }
